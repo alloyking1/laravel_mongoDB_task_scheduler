01d709133198aab618ab19779d5fb0483f154146
Jérôme Tamarelle <jerome@tamarelle.net>
2024-12-20 17:41:44 +0100

    User model must be stored in MongoDB, using the DocumentModel is required

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use MongoDB\Laravel\Eloquent\DocumentModel;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    use DocumentModel;
+
+    protected $connection = 'mongodb';
 
     /**
      * The attributes that are mass assignable.
